320985c2510b54786a16aa2754d3e1e5e50f5d15
Владислав Дыляев <xsinkinxsinkinx@gmail.com>
2021-04-20 21:54:22 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/ExceptionsCollection.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    class ExceptionsCollection : Exception
+    {
+        private Exception[] ex = new Exception[5];
+
+        public ExceptionsCollection()
+        {
+            ex[0] = new DivideByZeroException();
+            ex[1] = new DirectoryNotFoundException();
+            ex[2] = new ArgumentException();
+            ex[3] = new MyException("Моё исключение.");
+            ex[4] = new ArgumentNullException();
+        }
+        public Exception this[int index]
+        {
+            get
+            {
+                return ex[index];
+            }
+
+        }
+        public void ShowException()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                try
+                {
+                    throw ex[i];
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+
+                    Console.WriteLine();
+                }
+
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+namespace ConsoleApp1
+{
+ 
+    class Program
+    { 
+        static void Main(string[] args)
+        {
+            ExceptionsCollection exCollection = new ExceptionsCollection();
+            exCollection.ShowException();
+            
+            Console.ReadKey();
+
+        }
+    }
+}
